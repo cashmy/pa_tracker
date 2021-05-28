8006db17d1b48ea09faac0b76c4c1323dd3e513f
cashmy <cmyers880@gmail.com>
2021-05-28 18:04:58 -0500

    Deactivated Strict mode

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
