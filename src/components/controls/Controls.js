4a5a887519dde6b19c40c26cd4dbb95f06e6801b
cashmy <cmyers880@gmail.com>
2021-05-28 10:02:11 -0500

    Added RadioGroup wrapped component

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -2,12 +2,14 @@ import Button from './Button'
 import Checkbox from './Checkbox'
 import Input from './Input'
 import Select from './Select'
+import RadioGroup from './RadioGroup'
 
 const Controls = {
     Button,
     Checkbox,
     Input,
     Select,
+    RadioGroup,
 }
 
 export default Controls;
