582949705b514814ffb423d5cee724fee8393853
cashmy <cmyers880@gmail.com>
2021-06-02 15:39:26 -0500

    Removed Switch wrapped component - causing compile issues

--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -1,25 +1,25 @@
 import ActionButton from './ActionButton'
 import Button from './Button'
 import Checkbox from './Checkbox'
+import ConfirmDialog from './ConfirmDialog'
 import DatePicker from './DatePicker'
 import Input from './Input'
 import Notification from './Notification'
 import Popup from './Popup'
 import RadioGroup from './RadioGroup'
 import Select from './Select'
-// import Switch from './Switch'
 
 const Controls = {
     ActionButton,
     Button,
     Checkbox,
+    ConfirmDialog,
     DatePicker,
     Input,
     Notification,
     Popup,
     RadioGroup,
     Select,
-    // Switch,
 }
 
 export default Controls;
