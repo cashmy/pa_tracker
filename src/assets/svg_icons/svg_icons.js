5d098f513a850a9c420129fc7b8ed5ebcbcb1f0b
cashmy <cmyers880@gmail.com>
2021-06-04 21:50:08 -0500

    Refactored Drawer INTO App

---
+++ src/assets/svg_icons/svg_icons.js
@@ -0,0 +1,23 @@
+import { ReactComponent as ClinicIcon } from '../../assets/svg_icons/clinic.svg'; // Clinics
+import { ReactComponent as CPTIcon } from '../../assets/svg_icons/cpt.svg'; // CPT Codes
+import { ReactComponent as DiagIcon } from '../../assets/svg_icons/diagnosis.svg'; // Diagnosis Codes
+import { ReactComponent as NoteTypeIcon } from '../../assets/svg_icons/note_types.svg'; // NoteType Codes
+import { ReactComponent as PatientIcon } from '../../assets/svg_icons/patient.svg'; // Patients
+import { ReactComponent as POSIcon } from '../../assets/svg_icons/placeOfService.svg'; // Place of Service Codes
+import { ReactComponent as ProviderIcon } from '../../assets/svg_icons/provider.svg'; // Providers
+import { ReactComponent as SpecialtyIcon } from '../../assets/svg_icons/specialty.svg'; // Treatement-Therapy Codes
+import { ReactComponent as StatusIcon } from '../../assets/svg_icons/battery_status.svg'; // Status Codes
+
+const Svg_Icons = {
+    ClinicIcon,
+    CPTIcon,
+    DiagIcon,
+    NoteTypeIcon,
+    PatientIcon,
+    POSIcon,
+    ProviderIcon,
+    SpecialtyIcon,
+    StatusIcon
+}
+
+export default Svg_Icons;
