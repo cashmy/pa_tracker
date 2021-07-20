b94eed12a6b47fcb8a6838770cefa98e115017c5
cashmy <cmyers880@gmail.com>
2021-07-20 14:53:02 -0500

    Modified Store to include carrierSlice

--- src/App/store.jsx
+++ src/App/store.jsx
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiPriorAuthSlice } from "../features/priorAuth/priorAuthSlice";
+import { apiCarrierSlice } from "../features/carrierSlice";
 
 export const store = configureStore({
   reducer: {
     [apiPriorAuthSlice.reducerPath]: apiPriorAuthSlice.reducer,
+    [apiCarrierSlice.reducerPath]: apiCarrierSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(apiPriorAuthSlice.middleware);
+    return getDefaultMiddleware()
+      .concat(apiPriorAuthSlice.middleware)
+      .concat(apiCarrierSlice.middleware);
   },
 });
